0cdadd96dad215b16768dce66d7912897354340e
robobas <124291759+robobas@users.noreply.github.com>
2023-02-02 11:24:08 -0600

    Add files via upload

---
+++ Math Problem 31.2.3 version 2 ?.py
@@ -0,0 +1,8 @@
+meals = 14
+meals_per_person_per_day = 3
+days = 7
+meals_gotten_per_day = 4
+meals_gotten = days * meals_gotten_per_day
+total_meals = meals + meals_gotten
+amount_eaten_per_person_total = days * meals_per_person_per_day
+number_of_people_fed = total_meals / amount_eaten_per_person_total
